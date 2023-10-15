d1f8e7111f014f3fa317ea3afadd4ce5f30471da
mondyfy <khaptadisandip@gmail.com>
2023-10-15 13:08:05 +0545

    lint fixes

--- src/company/company.service.spec.ts
+++ src/company/company.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompanyService } from './company.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Company } from './entities/company.entity';
 
 describe('CompanyService', () => {
   let service: CompanyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CompanyService],
+      providers: [
+        CompanyService,
+        {
+          provide: getRepositoryToken(Company),
+          useValue: {
+            paginate: jest.fn(),
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CompanyService>(CompanyService);
--- src/upload/upload.controller.spec.ts
+++ src/upload/upload.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UploadController } from './upload.controller';
 import { UploadService } from './services/upload.service';
+import { Web3UploadService } from './services/web3_upload.service';
+import { CloudinaryService } from './services/cloudinary.service';
+import { CloudinaryProvider } from './providers/cloudinary.provider';
+import { S3Service } from './services/s3.service';
 
 describe('UploadController', () => {
   let controller: UploadController;
@@ -8,7 +12,13 @@ describe('UploadController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UploadController],
-      providers: [UploadService],
+      providers: [
+        UploadService,
+        Web3UploadService,
+        CloudinaryService,
+        CloudinaryProvider,
+        S3Service,
+      ],
     }).compile();
 
     controller = module.get<UploadController>(UploadController);
