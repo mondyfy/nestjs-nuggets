1ce9436724f4ee3873585d9082129e822187925b
mondyfy <khaptadisandip@gmail.com>
2023-10-16 10:35:25 +0545

    alphabetize endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('health')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000;
+  const docsRoute = process.env.API_DOCS_PREFIX || 'docs';
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('NestJS crash course API')
     .setDescription('Diving deep into Nests features')
     .setVersion('1.0')
-    .addTag('nestjs-nuggets')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup(docsRoute, app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
 
   await app.listen(port);
+  Logger.log(
+    'Swagger is ready on: ' + 'http://localhost:' + port + '/' + docsRoute,
+  );
   Logger.log('Application started on: ' + 'http://localhost:' + port);
 }
 bootstrap();
